60555c7793162aa8f9a151e8e49fd98c81350925
Ayananth <tsayananth93@gmail.com>
2026-06-12 13:53:55 +0530

    removed placeholder data

--- frontend/src/apis/venues.js
+++ frontend/src/apis/venues.js
@@ -1,4 +1,3 @@
-import { venues } from "../data/venues"
 import api from "../lib/axios"
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000"
@@ -44,7 +43,6 @@ function toExploreVenue(venue) {
 
 export async function fetchAllVenuesHome() {
   let { data } = await api.get("/venues/home", { baseURL: API_BASE_URL })
-  data = data.concat(venues)
   return data.map(toExploreVenue)
 }
 
