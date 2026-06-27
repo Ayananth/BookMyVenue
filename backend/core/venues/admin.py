1888c145f40d18a1b6debbc787ce23624c5ca8e1
Ayananth <tsayananth93@gmail.com>
2026-06-28 00:07:40 +0530

    model registration in admin

--- backend/core/venues/admin.py
+++ backend/core/venues/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Venue
+from .models import Venue, VenueSchedule, VenueScheduleGroup
 
 admin.site.register(Venue)
+admin.site.register(VenueSchedule)
+admin.site.register(VenueScheduleGroup)
+
