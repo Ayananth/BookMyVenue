a2eee18f1e27ad954b36aa735928b53d3e347ecf
Ayananth <tsayananth93@gmail.com>
2026-07-03 00:08:31 +0530

    added celery beat and worker for background tasks

--- backend/core/core/celery.py
+++ backend/core/core/celery.py
@@ -6,4 +6,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 app = Celery("bookmyvenue")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.beat_schedule = {
+    "expire-booking-sessions": {
+        "task": "expire_booking_sessions",
+        "schedule": 60.0,
+    },
+}
 app.autodiscover_tasks()
