ad558b72b4a3053fe72900187c96afb9f9d0411c
Ayananth <tsayananth93@gmail.com>
2026-07-18 13:56:00 +0530

    django endpoint to accept ratings

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path("bookings/", include("bookings.urls")),
     path("payments/", include("payments.urls")),
     path("contact/", include("contact.urls")),
+    path("internal/", include("venues.internal.urls")),
     path("uploads/image", ImageUploadView.as_view(), name="image-upload"),
 ]
 
